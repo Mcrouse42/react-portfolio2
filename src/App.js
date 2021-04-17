d83adf1a75af90ccb68f4c639a8dcf2e7d36eaa6
Mcrouse <matthew.crouse@temple.edu>
2021-04-16 23:12:48 -0400

    set up file structure and some components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "About",
+      component: <About/>,
+    },
+    { name: "Portfolio", component: <Portfolio/>},
+    { name: "Resume", component: <Resume/>},
+    {
+      name: "Contact" ,
+      component: <Contact/> ,
+    },
+  ]);
+  
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  // const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        // contactSelected={contactSelected}
+        // setContactSelected={setContactSelected}
+      />
+      <main>
+       
+       {currentCategory.component}
+            {/* <About>currentCategory={currentCategory}</About>
+            <Portfolio></Portfolio>
+            <Resume></Resume>
+          
+      
+          <ContactForm></ContactForm> */}
+       
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">About Me</h1>
+      {/* <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      /> */}
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </div>
+    </section>
+  );
+}
+
+export default About;
