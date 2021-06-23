888208bf22200c8871a6609f45eca7568a57cce9
Mcrouse <matthew.crouse@temple.edu>
2021-06-23 13:46:45 -0400

    updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 
+import resume from "../../assets/images/resume.png";
+
 function Resume() {
   const resumeLink = [
     {
-      img: resume,
       title: "Resume",
-      resume: "https://docs.google.com/document/d/1rSuNPxZc0065E5DItrRHPXNGRFVSuB3R500E1_ZuTnU/edit?usp=sharing",
+      img: resume,
+      resumeLink: "https://docs.google.com/document/d/1rSuNPxZc0065E5DItrRHPXNGRFVSuB3R500E1_ZuTnU/edit?usp=sharing",
     },
   ];
 
     return (
-      <div>
-        <h1>
-          <u>Resume</u>
-            </h1>
-            <h4>
-                Coming Soon
-            </h4>
+      <div className="flex-row">
+        {resumeLink.map((resume) => (
+          <div>
+                <h1>{resume.title}</h1>
+            <div className="flex-grid">
+              <img src={resume.img}></img>
+            </div>
+            <h5>
+              <a href={resume.resumeLink}>Resume Link</a>
+            </h5>
+          </div>
+        ))}
       </div>
     );
 }
