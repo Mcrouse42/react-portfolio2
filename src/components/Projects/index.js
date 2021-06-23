774e78accaf2bb3e43fecfbf0cc4123a66e9b141
Mcrouse <matthew.crouse@temple.edu>
2021-06-23 11:54:34 -0400

    updated projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,52 +2,36 @@ import React from "react";
 
 import developer from "../../assets/images/developer.png";
 import movers from "../../assets/images/movers.png";
-import weather from "../../assets/images/weather.png";
-import notetaker from "../../assets/images/notetaker.png";
-import book from "../../assets/images/book.png";
+import dungeon from "../../assets/images/dungeon.png";
 
 
 function Projects() {
     
-    const projectsArray =  [
-        {
-            img: developer,
-            title: "Developer Tracker",
-            description:
-                "A web application to track your progress as developer learning new technology.",
-            github: "https://github.com/Mcrouse42/developer-tracker-1",
-            deployed: "https://floating-reaches-76130.herokuapp.com/",
-        },
-        {
-            img: movers,
-            title: "Movers Not Shakers",
-            description:
-                "An application that allows you to view earthquakes near in proximity to your location.",
-            github: "https://github.com/Mcrouse42/movers-not-shakers",
-            deployed: "https://jbeedle19.github.io/movers-not-shakers/",
-        },
-        {
-            img: weather,
-            title: "Weather Dashboard",
-            description: "An app that allows you to track weather conditions.",
-            github: "https://github.com/Mcrouse42/weather-dashboard",
-            deployed: "https://mcrouse42.github.io/weather-dashboard/",
-        },
-        {
-            img: notetaker,
-            title: "Note taker",
-            description: "An app that allows you to write and store notes.",
-            github: "https://github.com/Mcrouse42/notetaker",
-            deployed: "https://evening-basin-46543.herokuapp.com/",
-        },
-        {
-            img: book,
-            title: "Book search engine",
-            description:
-                "An app that allows you to search for any book and save them.",
-            github: "https://github.com/Mcrouse42/book-search-engine",
-            deployed: "https://secure-coast-66975.herokuapp.com/",
-        },
+    const projectsArray = [
+      {
+        img: dungeon,
+        title: "Dungeon Master's Guide",
+        description:
+          "A website that allows you to track initiatives, players and monsters while you interact as a Dungeon Master for the popular game Dungeons and Dragons.",
+        github: "https://github.com/Mcrouse42/initiate-dnd",
+        deployed: "https://dungeon-masters-guide.herokuapp.com/",
+      },
+      {
+        img: developer,
+        title: "Developer Tracker",
+        description:
+          "A web application to track your progress as developer learning new technology.",
+        github: "https://github.com/Mcrouse42/developer-tracker-1",
+        deployed: "https://floating-reaches-76130.herokuapp.com/",
+      },
+      {
+        img: movers,
+        title: "Movers Not Shakers",
+        description:
+          "An application that allows you to view earthquakes near in proximity to your location.",
+        github: "https://github.com/Mcrouse42/movers-not-shakers",
+        deployed: "https://jbeedle19.github.io/movers-not-shakers/",
+      },
     ];
     
     
@@ -66,10 +50,6 @@ function Projects() {
               <a href={project.deployed}>Deployed</a>
             </h5>
           </div>
-          // <div>{projectsArray.title}</div>
-          // <div>projectsArray.description}</div>
-          // <div>{projectsArray.github}</div>
-          // <div>{projectsArray.deployed}</div>
         ))}
       </div>
     );
